5d264e372ae0d6decd3f5d548f2702ab75b30db9
Gregory Sedrez <gregorysedrez@gmail.com>
2025-02-18 23:11:35 -0300

    update: reset project with expo

---
+++ app/index.tsx
@@ -0,0 +1,48 @@
+// import { Text, View } from "react-native";
+
+// export default function Index() {
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         justifyContent: "center",
+//         alignItems: "center",
+//       }}
+//     >
+//       <Text>Hello World!!</Text>
+//       <Text>TO-DO LIST</Text>
+//       <Text>Chalenge</Text>
+//     </View>
+//   );
+// }
+
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const Flex = () => {
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          // Try setting `flexDirection` to `"row"`.
+          flexDirection: 'column',
+        },
+      ]}>
+        <Text >Teste</Text>
+      <View style={{flex: 2, backgroundColor: 'red'}}>
+      </View>
+      <View style={{flex: 2, backgroundColor: 'darkorange'}} />
+      <View style={{flex: 3, backgroundColor: 'green'}} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+});
+
+export default Flex;
